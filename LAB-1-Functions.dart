821f0aae114646c8d7d713dd2f05a4db0ba863f6
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 15:08:21 +0300

    Exercise-1 for functions

---
+++ LAB-1-Functions.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  double firstNumber = 20;
+  double secondNumber = 30;
+  double sum= adder(firstNumber, secondNumber);
+
+  
+  print("The sum of $firstNumber and $secondNumber is: $sum");
+}
+
+
+double adder(double a, double b) {
+  return a + b;
+}
