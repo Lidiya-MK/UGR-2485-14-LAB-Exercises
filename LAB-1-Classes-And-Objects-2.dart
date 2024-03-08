3ea66efd4966e4bae3ff896c95ad71539cc82bdb
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 16:14:44 +0300

    Exercise-2 from the first classes and objects slide

--- LAB-1-Classes-And-Objects-2.dart
+++ LAB-1-Classes-And-Objects-2.dart
@@ -4,33 +4,57 @@ class Person {
   String address;
   String occupation;
 
-  
   Person(this.name, this.age, this.address, this.occupation);
 
-
   void displayInfo() {
-  
-    print("The persons name is  $name. They are $age years old. They are currently living in $address and their occupation is $occupation.");
+    print("The person's name is $name. They are $age years old. They are currently living in $address, and they are a  $occupation.");
+  }
+}
+
+class Student extends Person {
+  int rollNumber;
+  List<int> marks;
+
+
+  Student(String name, int age, String address, String occupation, this.rollNumber, this.marks)
+      : super(name, age, address, occupation);
+
+
+  int totalMark() {
+    return marks.reduce((mark1, mark2) => mark1 + mark2);
+  }
+
+  // Method to calculate average marks
+  double averageMark() {
+    if (marks.isNotEmpty) {
+      return totalMark() / marks.length;
+    } else {
+      return 0.0;
+    }
+  }
+
+  // Override displayInfo to include student-specific information
+  @override
+  void displayInfo() {
+    super.displayInfo();
+    print("Roll Number: $rollNumber");
+    print("Marks: $marks");
   }
 }
 
 void main() {
+  // Create Student objects
+  Student student1 = Student("Abebe Beso", 20, "WoloSefer", "Student", 101, [85, 90, 92]);
+  Student student2 = Student("Chala Daba", 22, "Kasanches", "Student", 102, [78, 85, 90]);
 
-  Person person1 = Person("Abebe Beso", 30, "WoloSefer", "Driver");
-  Person person2 = Person("Chala Daba", 25, "Kasanches","Surgeon");
-
-
-
-  person1.displayInfo();
+  // Display student information
+  print("Initial Student Information:");
+  student1.displayInfo();
   print("\n");
-  person2.displayInfo();
+  student2.displayInfo();
   print("\n");
 
-  
-  person1.age = 37;
-  person2.address = "Adey Ababa";
-
-  person1.displayInfo();
-  print("\n");
-  person2.displayInfo();
+  // Calculate and print total and average marks
+  print("${student1.name}- Total Marks: ${student1.totalMark()}, Average Marks: ${student1.averageMark().toStringAsFixed(2)}");
+  print("${student2.name} - Total Marks: ${student2.totalMark()}, Average Marks: ${student2.averageMark().toStringAsFixed(2)}");
 }
