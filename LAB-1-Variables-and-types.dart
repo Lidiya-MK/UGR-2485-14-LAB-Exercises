7a8cf5018195c1576b8d4f1258c12a8394810212
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 14:36:31 +0300

    Exercise-1 for variables and data types

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  
+  String name = "Lidiya Mamo Kibret";
+  int age = 20;
+  String favoriteColor = "Burgandy"; 
+
+  print("My name is  $name. I am  $age years old and $favoriteColor is my favorite color.");
+
+}
