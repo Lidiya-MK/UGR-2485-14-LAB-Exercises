41dd5c1e2b862f85590875e4a51090d4c0b578b1
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 17:06:45 +0300

    Exercise3 for error handling

--- LAB-1-Error-Handling.dart
+++ LAB-1-Error-Handling.dart
@@ -1,16 +1,11 @@
-double divideNumbers(double numerator, double denominator) {
-  try {
-    if (denominator == 0) {
-      throw IntegerDivisionByZeroException(); // or DoubleDivisionByZeroException()
-    }
-    return numerator / denominator;
-  } catch (e) {
-    print('Error: Division by zero');
-    return double.nan;
-  }
-}
+import 'dart:io';
 
 void main() {
-  double result = divideNumbers(10, 0);
-  print('Result: $result');
+  try {
+    File file = File('sample.txt');
+    String content = file.readAsStringSync();
+    print('File content: $content');
+  } catch (e) {
+    print('Error: ${e.toString()}');
+  }
 }
