608f020789e48c89b1a00da920449e1f39ca7a7d
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 16:42:03 +0300

    Exercise3 for async programming

--- LAB-1-Async-programming.dart
+++ LAB-1-Async-programming.dart
@@ -1,30 +1,12 @@
-import 'dart:io';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-Future<void> downloadFile(String url, String savePath) async {
-  try {
-    // Send a GET request to the URL
-    final response = await http.get(Uri.parse(url));
-
-    // Check if the request was successful (status code 200)
-    if (response.statusCode == 200) {
-      // Write the downloaded content to a file
-      File(savePath).writeAsBytesSync(response.bodyBytes);
-      print('Download complete. File saved at: $savePath');
-    } else {
-      print('Failed to download file. Status code: ${response.statusCode}');
-    }
-  } catch (e) {
-    print('Error: $e');
-  }
+Future<List<String>> fetchDataFromDatabase() async {
+  await Future.delayed(Duration(seconds: 2));
+  return ['Data1', 'Data2', 'Data3'];
 }
 
 void main() async {
-  // Replace 'YOUR_FILE_URL' and 'YOUR_SAVE_PATH' with your file URL and desired save path
-  String fileUrl = 'https://irp-cdn.multiscreensite.com/cb9165b2/files/uploaded/The+48+Laws+Of+Power.pdf';
-  String savePath = 'C:\\';
+  print('Loading data from the database...');
 
-  print('Downloading file...');
-  await downloadFile(fileUrl, savePath);
+  List<String> data = await fetchDataFromDatabase();
+
+  print('Loaded data: $data');
 }
