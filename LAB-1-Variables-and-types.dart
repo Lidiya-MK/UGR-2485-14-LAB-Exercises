567d0f51fc63beeb6b90e0a76fbfd1c66f314d39
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 14:40:55 +0300

    Exercise-2 for variables and data types

--- LAB-1-Variables-and-types.dart
+++ LAB-1-Variables-and-types.dart
@@ -1,9 +1,14 @@
+//Exercise 2 for variables and data types
+import "dart:io";
 void main() {
-  
-  String name = "Lidiya Mamo Kibret";
-  int age = 20;
-  String favoriteColor = "Burgandy"; 
+  const int speedOfLight = 299792458;
 
-  print("My name is  $name. I am  $age years old and $favoriteColor is my favorite color.");
+  print("Enter the distance in meters:");
+  String userInput = stdin.readLineSync() ?? '';
+  double distance = double.tryParse(userInput) ?? 0.0;
 
+  double time = distance / speedOfLight;
+
+  print("Time for light to travel $distance meters: $time seconds");
 }
+
