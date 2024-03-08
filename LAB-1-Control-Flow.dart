f155a6414b71c349bc6c7a010f3ca9166808c3ea
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 14:55:48 +0300

    Exercise-4 for control flow statements

--- LAB-1-Control-Flow.dart
+++ LAB-1-Control-Flow.dart
@@ -1,7 +1,44 @@
-//Exercise 2 for conrol flow statements
+//Exercise 4 for conrol flow statementsimport 'dart:io';
+import "dart:io";
 void main() {
 
-  for (int i = 1; i <= 10; i++) {
-    print(i);
+  print("Enter the first number:");
+  double firstNum = double.tryParse(stdin.readLineSync() ?? '') ?? 0.0;
+
+
+  print("Enter the operation (+, -, *, /):");
+  String operation = stdin.readLineSync() ?? '';
+
+  print("Enter the second number:");
+  double secondNum = double.tryParse(stdin.readLineSync() ?? '') ?? 0.0;
+
+
+  double result = 0.0;
+  switch (operation) {
+    case '+':
+      result = firstNum + secondNum;
+      break;
+    case '-':
+      result = firstNum - secondNum;
+      break;
+    case '*':
+      result = firstNum * secondNum;
+      break;
+    case '/':
+      
+      if (secondNum != 0) {
+        result = firstNum / secondNum;
+      } else {
+        print("OOPs!! cannot divide by Zero");
+        return;
+      }
+      break;
+    default:
+      print("This operation is not recognized by the calculator. Please enter +, -, *, or /.");
+      return;
   }
+
+
+  print("Result: $result");
 }
+
