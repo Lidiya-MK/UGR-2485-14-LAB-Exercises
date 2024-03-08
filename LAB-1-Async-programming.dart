0e31076d6569bde95c3cad04635bb9c46ffcd41f
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 16:53:09 +0300

    Exercise4 for async programming

--- LAB-1-Async-programming.dart
+++ LAB-1-Async-programming.dart
@@ -1,12 +1,41 @@
-Future<List<String>> fetchDataFromDatabase() async {
-  await Future.delayed(Duration(seconds: 2));
-  return ['Data1', 'Data2', 'Data3'];
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> fetchWeatherData() async {
+  try {
+
+    String apiKey = 'b1b15e88fa797225412429c1c50c122a1">api.openweathermap.org/data/2.5/forecast?id&appid=3ac79e32091412d26b67e760115c5610';
+    String city = 'New York'; 
+
+   
+    final response = await http.get(
+      Uri.parse('b1b15e88fa797225412429c1c50c122a1">api.openweathermap.org/data/2.5/forecast?id&appid=3ac79e32091412d26b67e760115c5610'),
+    );
+
+  
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+
+
+      double temperature = data['main']['temp'] - 273.15; 
+      String conditions = data['weather'][0]['description'];
+
+      print('Current Temperature: ${temperature.toStringAsFixed(2)}°C');
+      print('Weather Conditions: $conditions');
+    } else {
+      print('Failed to fetch weather data. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
 }
 
 void main() async {
-  print('Loading data from the database...');
+  print('Fetching weather data...');
 
-  List<String> data = await fetchDataFromDatabase();
-
-  print('Loaded data: $data');
+  await fetchWeatherData();
 }
+
+
+
+
