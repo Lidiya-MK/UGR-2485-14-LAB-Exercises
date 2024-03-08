74d6a2fffeecd9860a6575e91b1a6d05407c9478
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 14:50:13 +0300

    Exercise-2 for control flow statements

--- LAB-1-Control-Flow.dart
+++ LAB-1-Control-Flow.dart
@@ -1,15 +1,7 @@
-//Exercise 1 for conrol flow statements
-import 'dart:io';
-
+//Exercise 2 for conrol flow statements
 void main() {
-  print("Enter an integer you would like to check:");
-  String userInput = stdin.readLineSync() ?? '';
-  int number = int.tryParse(userInput) ?? 0;
 
-
-  if (!(number % 2 == 0)) {
-    print("$number is odd.");
-  } else {
-    print("$number is even.");
+  for (int i = 1; i <= 10; i++) {
+    print(i);
   }
 }
