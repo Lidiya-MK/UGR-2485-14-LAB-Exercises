4549044362947aa625c04f977b9ac50236ebc065
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-03-08 15:04:03 +0300

    Exercise-5 for control flow statements

--- LAB-1-Control-Flow.dart
+++ LAB-1-Control-Flow.dart
@@ -1,44 +1,47 @@
-//Exercise 4 for conrol flow statementsimport 'dart:io';
-import "dart:io";
+//**For this exercise I just assumed the number grades on a 4.0 scale
+import 'dart:io';
+
 void main() {
 
-  print("Enter the first number:");
-  double firstNum = double.tryParse(stdin.readLineSync() ?? '') ?? 0.0;
+  print("Enter your grade:");
+  double numberGrade = double.tryParse(stdin.readLineSync() ?? '') ?? 0.0;
 
-
-  print("Enter the operation (+, -, *, /):");
-  String operation = stdin.readLineSync() ?? '';
-
-  print("Enter the second number:");
-  double secondNum = double.tryParse(stdin.readLineSync() ?? '') ?? 0.0;
-
-
-  double result = 0.0;
-  switch (operation) {
-    case '+':
-      result = firstNum + secondNum;
+  String letterGrade = '';
+  switch (numberGrade) {
+    case 4.0:
+      letterGrade = 'A';
       break;
-    case '-':
-      result = firstNum - secondNum;
+    case 3.75:
+      letterGrade = 'A-';
       break;
-    case '*':
-      result = firstNum * secondNum;
+    case 3.5:
+      letterGrade = 'B+';
       break;
-    case '/':
-      
-      if (secondNum != 0) {
-        result = firstNum / secondNum;
-      } else {
-        print("OOPs!! cannot divide by Zero");
-        return;
-      }
+    case 3.0:
+      letterGrade = 'B';
+      break;
+    case 2.75:
+      letterGrade = 'B-';
+      break;
+    case 2.5:
+      letterGrade = 'C+';
+      break;
+    case 2.0:
+      letterGrade = 'C';
+      break;
+    case 1.75:
+      letterGrade = 'C-';
+      break;
+    case 1.0:
+      letterGrade = 'D';
+      break;
+    case 0.0:
+      letterGrade = 'F';
       break;
     default:
-      print("This operation is not recognized by the calculator. Please enter +, -, *, or /.");
+      print("Please enter a valid grade number.");
       return;
   }
 
-
-  print("Result: $result");
+  print("Your grade is: $letterGrade");
 }
-
